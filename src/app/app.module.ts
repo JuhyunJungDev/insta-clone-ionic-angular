904fafac6cb38770f447e5d2644f13c259369810
JuhyunDevBlesso <jung.juhyun0503@gmail.com>
2024-10-21 14:49:53 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './widget/user/user.component';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -6,6 +6,11 @@ import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { PostComponent } from '../widget/post/post.component';
+import { UserComponent } from '../widget/user/user.component';
+import { CommentComponent } from '../widget/comment/comment.component';
+import { StoryComponent } from '../widget/story/story.component';
+import { HeaderComponent } from '../widget/header/header.component';
 
 @NgModule({
   imports: [
@@ -15,6 +20,6 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page, PostComponent, UserComponent, CommentComponent, StoryComponent, HeaderComponent]
 })
 export class Tab1PageModule {}
